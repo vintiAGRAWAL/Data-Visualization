5db75724dbe85a2eabdcff6a1a78dd8cc49487bc
Vinti <vintiagrawal275@gmail.com>
2025-03-30 14:17:18 +0530

    Add Draggable Table for Project Management

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import "./App.css";
 import { LineGraph } from "./components/Line";
 import { BarChart } from "./components/Bar";
 import { PieChart } from "./components/Pie";
+import { DraggableTable } from "./components/DraggableTable";
 
 function App() {
   return (
     <div className="Apps">
-      <LineGraph />
+      {/* <LineGraph />
       <br />
       <BarChart />
       <br />
       <PieChart />
+      <br /> */}
+      <DraggableTable />
     </div>
   );
 }
---
+++ src/components/Column.js
@@ -0,0 +1,18 @@
+import { Task } from "./Task";
+import { useDroppable } from "@dnd-kit/core";
+export const Column = ({ column, tasks }) => {
+  const { setNodeRef } = useDroppable({ id: column.id });
+  return (
+    <div
+      ref={setNodeRef}
+      className="flex w-80 flex-col rounded-lg bg-neutral-800 p-4"
+    >
+      <h2 className="mb-4 font-semibold text-neutral-400">{column.title}</h2>
+      <div className="flex flex-1 flex-col gap-4">
+        {tasks.map((task) => {
+          return <Task key={task.id} task={task}></Task>;
+        })}
+      </div>
+    </div>
+  );
+};
